591325bbb89ff264a2732d28c433171a81b71b58
nishijima <54151370+toppy777@users.noreply.github.com>
2025-03-22 12:37:32 +0000

    #3 テスト環境構築

---
+++ __test__/page.test.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { Home } from '../src/app/page'
+import { User } from '../src/app/lib/users'
+ 
+describe('Home', () => {
+
+    const users: User[] = [
+        {
+            id: 1,
+            email: 'hogehoge.example.com',
+            name: 'hogehoge'
+        },
+        {
+            id: 2,
+            email: 'fugafuga.example.com',
+            name: 'fugafuga'
+        },
+    ]
+
+  it('renders users', () => {
+    render(<Home users={users} />)
+    expect(screen.getByText('Users: 2')).toBeInTheDocument()
+  })
+})
