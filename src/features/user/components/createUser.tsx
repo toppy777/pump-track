4bddc6ab3544354bd68798ff501f6f743f3c3006
nishijima <54151370+toppy777@users.noreply.github.com>
2025-03-23 03:44:44 +0000

    #8 eslintに引っかかった部分を削除

--- src/features/user/components/createUser.tsx
+++
@@ -1,9 +0,0 @@
-import { createUser } from "../user"
-
-export default function CreateUser() {
-    return (
-        <div>
-
-        </div>
-    )
-}
--- src/features/user/user.ts
+++ src/features/user/user.ts
@@ -9,11 +9,3 @@ export type User = {
 export async function getUsers() : Promise<User[]> {
   return await prisma.user.findMany()
 }
-
-export async function createUser(email: string, name: string) {
-  return await prisma.user.create({
-    data: {
-      email: email,
-      name: name
-    }})
-}
