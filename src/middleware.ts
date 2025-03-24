0d0901e0231157cabf10c594d3001a058703b285
nishijima <54151370+toppy777@users.noreply.github.com>
2025-03-24 02:34:54 +0000

    #8 デプロイが失敗したので修正4

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,10 @@
 import { auth } from "@/features/auth/config"
-import { NextRequest } from "next/server"
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
  
-export default auth((req: NextRequest) => {
+export default auth((req) => {
   if (!req.auth && req.nextUrl.pathname !== "/") {
     return Response.redirect(req.nextUrl.origin)
   }
