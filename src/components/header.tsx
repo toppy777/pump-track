72b0c62491097690db96c29b2ac76f9c312ba20a
nishijima <54151370+toppy777@users.noreply.github.com>
2025-04-07 06:31:51 +0000

    #46 個別トレーニングページに一覧に戻るリンク、ヘッダーにナビゲーションリンクを追加した

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,11 @@
 import UserAvatar from '@/features/auth/components/UserAvatar'
 import { Session } from 'next-auth'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Header({ session }: { session: Session }) {
+  const pathname = usePathname()
+
   return (
     <header className="h-[6svh]">
       <nav className="nav-bar flex items-center justify-between h-full">
@@ -16,6 +19,33 @@ export default function Header({ session }: { session: Session }) {
           >
             pump track
           </Link>
+        </div>
+        <div className="flex flex-row">
+          <div className="pr-[5svw] flex items-center h-[4svh]">
+            ダッシュボード
+          </div>
+          <div className="pr-[5svw] flex items-center h-[4svh]">
+            <Link
+              href="/trainings"
+              className={
+                'px-[2svw] py-[0.5svh] ' +
+                (pathname === '/trainings' ? 'nav-current' : '')
+              }
+            >
+              トレーニング
+            </Link>
+          </div>
+          <div className="pr-[5svw] flex items-center h-[4svh]">
+            <Link
+              href="/exercises"
+              className={
+                'px-[2svw] py-[0.5svh] ' +
+                (pathname === '/exercises' ? 'nav-current' : '')
+              }
+            >
+              種目
+            </Link>
+          </div>
         </div>
         <div className="pr-[5svw] flex items-center h-[4svh]">
           {session !== null && session.user !== undefined ? (
